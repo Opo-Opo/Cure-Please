356f592e80f2d044e1eb4d6634c9d9de5298f51e
Opo-Opo <20958729+Opo-Opo@users.noreply.github.com>
2022-02-25 21:57:15 +0000

    Add Monitored Party Members to Debuff Removal Spell List

--- CurePlease.App/DebuffRemovalSpell.cs
+++ CurePlease.App/DebuffRemovalSpell.cs
@@ -3,179 +3,389 @@ using EliteMMO.API;
 
 namespace CurePlease.App
 {
-    public record DebuffRemovalSpell(string Name, bool PlEnabled, bool MonitoredEnabled)
+    public record DebuffRemovalSpell(string Name, bool PlEnabled, bool MonitoredEnabled, bool PartyEnabled)
     {
         public static Dictionary<StatusEffect, DebuffRemovalSpell> DebuffList()
             => new()
             {
                 {
                     StatusEffect.Doom,
-                    new("Cursna", OptionsForm.config.plDoom, OptionsForm.config.monitoredDoom)
+                    new(
+                        "Cursna",
+                        OptionsForm.config.plDoom,
+                        OptionsForm.config.monitoredDoom,
+                        OptionsForm.config.naCurse
+                    )
                 },
                 {
                     StatusEffect.Paralysis,
-                    new("Paralyna", OptionsForm.config.plParalysis, OptionsForm.config.monitoredParalysis)
+                    new(
+                        "Paralyna",
+                        OptionsForm.config.plParalysis,
+                        OptionsForm.config.monitoredParalysis,
+                        OptionsForm.config.naParalysis
+                    )
                 },
                 //{StatusEffect.Amnesia, "Esuna", OptionsForm.config.plAmnesia, OptionsForm.config.monitoredAmnesia}, //BuffChecker(0, 418)
                 {
                     StatusEffect.Poison,
-                    new("Poisona", OptionsForm.config.plPoison, OptionsForm.config.monitoredPoison)
+                    new(
+                        "Poisona",
+                        OptionsForm.config.plPoison,
+                        OptionsForm.config.monitoredPoison,
+                        OptionsForm.config.naPoison
+                    )
                 },
                 {
                     StatusEffect.Attack_Down,
-                    new("Erase", OptionsForm.config.plAttackDown, OptionsForm.config.monitoredAttackDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plAttackDown,
+                        OptionsForm.config.monitoredAttackDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_AttackDown
+                    )
                 },
                 {
                     StatusEffect.Blindness,
-                    new("Blindna", OptionsForm.config.plBlindness, OptionsForm.config.monitoredBlindness)
+                    new(
+                        "Blindna",
+                        OptionsForm.config.plBlindness,
+                        OptionsForm.config.monitoredBlindness,
+                        OptionsForm.config.naBlindness
+                    )
                 },
                 {
                     StatusEffect.Bind,
-                    new("Erase", OptionsForm.config.plAttackDown, OptionsForm.config.monitoredAttackDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plAttackDown,
+                        OptionsForm.config.monitoredAttackDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Bind
+                    )
                 },
                 {
                     StatusEffect.Weight,
-                    new("Erase", OptionsForm.config.plWeight, OptionsForm.config.monitoredWeight)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plWeight,
+                        OptionsForm.config.monitoredWeight,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Weight
+                    )
                 },
                 {
                     StatusEffect.Slow,
-                    new("Erase", OptionsForm.config.plSlow, OptionsForm.config.monitoredSlow)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plSlow,
+                        OptionsForm.config.monitoredSlow,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Slow
+                    )
                 },
                 {
                     StatusEffect.Curse,
-                    new("Cursna", OptionsForm.config.plCurse, OptionsForm.config.monitoredCurse)
+                    new(
+                        "Cursna",
+                        OptionsForm.config.plCurse,
+                        OptionsForm.config.monitoredCurse,
+                        OptionsForm.config.naCurse
+                    )
                 },
                 {
                     StatusEffect.Curse2,
-                    new("Cursna", OptionsForm.config.plCurse2, OptionsForm.config.monitoredCurse2)
+                    new(
+                        "Cursna",
+                        OptionsForm.config.plCurse,
+                        OptionsForm.config.monitoredCurse,
+                        OptionsForm.config.naCurse
+                    )
                 },
                 {
                     StatusEffect.Addle,
-                    new("Erase", OptionsForm.config.plAddle, OptionsForm.config.monitoredAddle)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plAddle,
+                        OptionsForm.config.monitoredAddle,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Addle
+                    )
                 },
                 {
                     StatusEffect.Bane,
-                    new("Cursna", OptionsForm.config.plBane, OptionsForm.config.monitoredBane)
+                    new(
+                        "Cursna",
+                        OptionsForm.config.plBane,
+                        OptionsForm.config.monitoredBane,
+                        OptionsForm.config.naCurse
+                    )
                 },
                 {
                     StatusEffect.Plague,
-                    new("Viruna", OptionsForm.config.plPlague, OptionsForm.config.monitoredPlague)
+                    new(
+                        "Viruna",
+                        OptionsForm.config.plPlague,
+                        OptionsForm.config.monitoredPlague,
+                        OptionsForm.config.naDisease
+                    )
                 },
                 {
                     StatusEffect.Disease,
-                    new("Viruna", OptionsForm.config.plDisease, OptionsForm.config.monitoredDisease)
+                    new(
+                        "Viruna",
+                        OptionsForm.config.plDisease,
+                        OptionsForm.config.monitoredDisease,
+                        OptionsForm.config.naDisease
+                    )
                 },
                 {
                     StatusEffect.Burn,
-                    new("Erase", OptionsForm.config.plBurn, OptionsForm.config.monitoredBurn)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plBurn,
+                        OptionsForm.config.monitoredBurn,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Burn
+                    )
                 },
                 {
                     StatusEffect.Frost,
-                    new("Erase", OptionsForm.config.plFrost, OptionsForm.config.monitoredFrost)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plFrost,
+                        OptionsForm.config.monitoredFrost,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Frost
+                    )
                 },
                 {
                     StatusEffect.Choke,
-                    new("Erase", OptionsForm.config.plChoke, OptionsForm.config.monitoredChoke)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plChoke,
+                        OptionsForm.config.monitoredChoke,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Choke
+                    )
                 },
                 {
                     StatusEffect.Rasp,
-                    new("Erase", OptionsForm.config.plRasp, OptionsForm.config.monitoredRasp)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plRasp,
+                        OptionsForm.config.monitoredRasp,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Rasp
+                    )
                 },
                 {
                     StatusEffect.Shock,
-                    new("Erase", OptionsForm.config.plShock, OptionsForm.config.monitoredShock)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plShock,
+                        OptionsForm.config.monitoredShock,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Shock
+                    )
                 },
                 {
                     StatusEffect.Drown,
-                    new("Erase", OptionsForm.config.plAttackDown, OptionsForm.config.monitoredAttackDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plAttackDown,
+                        OptionsForm.config.monitoredAttackDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Drown
+                    )
                 },
                 {
                     StatusEffect.Dia,
-                    new("Erase", OptionsForm.config.plDia, OptionsForm.config.monitoredDia)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plDia,
+                        OptionsForm.config.monitoredDia,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Dia
+                    )
                 },
                 {
                     StatusEffect.Bio,
-                    new("Erase", OptionsForm.config.plBio, OptionsForm.config.monitoredBio)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plBio,
+                        OptionsForm.config.monitoredBio,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Bio
+                    )
                 },
                 {
                     StatusEffect.STR_Down,
-                    new("Erase", OptionsForm.config.plStrDown, OptionsForm.config.monitoredStrDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plStrDown,
+                        OptionsForm.config.monitoredStrDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_StrDown
+                    )
                 },
                 {
                     StatusEffect.DEX_Down,
-                    new("Erase", OptionsForm.config.plDexDown, OptionsForm.config.monitoredDexDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plDexDown,
+                        OptionsForm.config.monitoredDexDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_DexDown
+                    )
                 },
                 {
                     StatusEffect.VIT_Down,
-                    new("Erase", OptionsForm.config.plVitDown, OptionsForm.config.monitoredVitDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plVitDown,
+                        OptionsForm.config.monitoredVitDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_VitDown
+                    )
                 },
                 {
                     StatusEffect.AGI_Down,
-                    new("Erase", OptionsForm.config.plAgiDown, OptionsForm.config.monitoredAgiDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plAgiDown,
+                        OptionsForm.config.monitoredAgiDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_AgiDown
+                    )
                 },
                 {
                     StatusEffect.INT_Down,
-                    new("Erase", OptionsForm.config.plIntDown, OptionsForm.config.monitoredIntDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plIntDown,
+                        OptionsForm.config.monitoredIntDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_IntDown
+                    )
                 },
                 {
                     StatusEffect.MND_Down,
-                    new("Erase", OptionsForm.config.plMndDown, OptionsForm.config.monitoredMndDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plMndDown,
+                        OptionsForm.config.monitoredMndDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_MndDown
+                    )
                 },
                 {
                     StatusEffect.CHR_Down,
-                    new("Erase", OptionsForm.config.plChrDown, OptionsForm.config.monitoredChrDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plChrDown,
+                        OptionsForm.config.monitoredChrDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_ChrDown
+                    )
                 },
                 {
                     StatusEffect.Max_HP_Down,
-                    new("Erase", OptionsForm.config.plMaxHpDown, OptionsForm.config.monitoredMaxHpDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plMaxHpDown,
+                        OptionsForm.config.monitoredMaxHpDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_MaxHpDown
+                    )
                 },
                 {
                     StatusEffect.Max_MP_Down,
-                    new("Erase", OptionsForm.config.plMaxMpDown, OptionsForm.config.monitoredMaxMpDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plMaxMpDown,
+                        OptionsForm.config.monitoredMaxMpDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_MaxMpDown
+                    )
                 },
                 {
                     StatusEffect.Accuracy_Down,
-                    new("Erase", OptionsForm.config.plAccuracyDown, OptionsForm.config.monitoredAccuracyDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plAccuracyDown,
+                        OptionsForm.config.monitoredAccuracyDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_MagicAccDown
+                    )
                 },
                 {
                     StatusEffect.Evasion_Down,
-                    new("Erase", OptionsForm.config.plEvasionDown, OptionsForm.config.monitoredEvasionDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plEvasionDown,
+                        OptionsForm.config.monitoredEvasionDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_EvasionDown
+                    )
                 },
                 {
                     StatusEffect.Defense_Down,
-                    new("Erase", OptionsForm.config.plDefenseDown, OptionsForm.config.monitoredDefenseDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plDefenseDown,
+                        OptionsForm.config.monitoredDefenseDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_DefenseDown
+                    )
                 },
                 {
                     StatusEffect.Flash,
-                    new("Erase", OptionsForm.config.plFlash, OptionsForm.config.monitoredFlash)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plFlash,
+                        OptionsForm.config.monitoredFlash,
+                        OptionsForm.config.naErase
+                    )
                 },
                 {
                     StatusEffect.Magic_Acc_Down,
-                    new("Erase", OptionsForm.config.plMagicAccDown, OptionsForm.config.monitoredMagicAccDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plMagicAccDown,
+                        OptionsForm.config.monitoredMagicAccDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_MagicAccDown
+                    )
                 },
                 {
                     StatusEffect.Magic_Atk_Down,
-                    new("Erase", OptionsForm.config.plMagicAtkDown, OptionsForm.config.monitoredMagicAtkDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plMagicAtkDown,
+                        OptionsForm.config.monitoredMagicAtkDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_MagicAttackDown
+                    )
                 },
                 {
                     StatusEffect.Helix,
-                    new("Erase", OptionsForm.config.plHelix, OptionsForm.config.monitoredHelix)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plHelix,
+                        OptionsForm.config.monitoredHelix,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Helix
+                    )
                 },
                 {
                     StatusEffect.Max_TP_Down,
-                    new("Erase", OptionsForm.config.plMaxTpDown, OptionsForm.config.monitoredMaxTpDown)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plMaxTpDown,
+                        OptionsForm.config.monitoredMaxTpDown,
+                        OptionsForm.config.naErase && OptionsForm.config.na_MaxTpDown
+                    )
                 },
                 {
                     StatusEffect.Requiem,
-                    new("Erase", OptionsForm.config.plRequiem, OptionsForm.config.monitoredRequiem)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plRequiem,
+                        OptionsForm.config.monitoredRequiem,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Requiem
+                    )
                 },
                 {
                     StatusEffect.Elegy,
-                    new("Erase", OptionsForm.config.plElegy, OptionsForm.config.monitoredElegy)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plElegy,
+                        OptionsForm.config.monitoredElegy,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Elegy
+                    )
                 },
                 {
                     StatusEffect.Threnody,
-                    new("Erase", OptionsForm.config.plThrenody, OptionsForm.config.monitoredThrenody)
+                    new(
+                        "Erase",
+                        OptionsForm.config.plThrenody,
+                        OptionsForm.config.monitoredThrenody,
+                        OptionsForm.config.naErase && OptionsForm.config.na_Threnody
+                    )
                 },
             };
     }
